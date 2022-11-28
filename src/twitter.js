5b4c7af9b57ee0d404444323e4c126f6e05cf7f0
Zach Leatherman <zachleatherman@gmail.com>
2022-11-28 13:03:55 -0600

    Use <is-land> to load opengraph images (which may not exist and are removed if so)

--- src/twitter.js
+++ src/twitter.js
@@ -155,7 +155,7 @@ class Twitter {
 					text = text.replace(url.url, displayUrlHtml);
 
 					if(targetUrl.startsWith("https://") && !targetUrl.startsWith("https://twitter.com/")) {
-						medias.push(`<a href="${targetUrl}"><img src="https://v1.opengraph.11ty.dev/${encodeURIComponent(targetUrl)}/small/" alt="OpenGraph image for ${displayUrl}" loading="lazy" decoding="async" width="375" height="197" class="tweet-media tweet-media-og"></a>`);
+						medias.push(`<template data-island><a href="${targetUrl}"><img src="https://v1.opengraph.11ty.dev/${encodeURIComponent(targetUrl)}/small/onerror/" alt="OpenGraph image for ${displayUrl}" loading="lazy" decoding="async" width="375" height="197" class="tweet-media tweet-media-og" onerror="this.parentNode.remove()"></a></template>`);
 					}
 				}
 			}
@@ -202,7 +202,7 @@ class Twitter {
 			}
 		}
 		if(medias.length) {
-			text += `<div class="tweet-medias">${medias.join("")}</div>`;
+			text += `<is-land on:visible><div class="tweet-medias">${medias.join("")}</div></is-land>`;
 		}
 		return text;
 	}
